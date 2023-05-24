954e02998d42f0e9a4de813df259c8c64519a033
Esteban Russo <oerusso28@gmail.com>
2023-05-24 00:35:05 -0300

    validamos todos los datos

--- controladores/clientes.controlador.php
+++ controladores/clientes.controlador.php
@@ -9,10 +9,56 @@ class ControladorClientes{
 
     public function create($datos){
 
+        /*===============================
+            Validar nombre
+         ===============================*/
 
-        echo '<pre>'; print_r($datos); echo '</pre>';
-        
-        return;
+        if (isset($datos['nombre']) && !preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/',$datos['nombre'])){
+            
+            $json = array(
+             'status'=>404,
+             'detalle'=>'Error en el campo nombre, solo se permiten letras'
+                
+            );
+            
+            echo json_encode($json, true);   
+            
+            return;
+        } 
+
+         /*===============================
+            Validar apellido
+         ===============================*/
+
+         if (isset($datos['apellido']) && !preg_match('/^[a-zA-ZáéíóúÁÉÍÓÚñÑ]+$/',$datos['apellido'])){
+            
+            $json = array(
+             'status'=>404,
+             'detalle'=>'Error en el campo apellido, solo se permiten letras'
+                
+            );
+            
+            echo json_encode($json, true);   
+            
+            return;
+        } 
+
+        /*===============================
+            Validar email
+         ===============================*/
+
+         if (isset($datos['apellido']) && !preg_match('/^[^0-9][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[@][a-zA-Z0-9_]+([.][a-zA-Z0-9_]+)*[.][a-zA-Z]{2,4}$/',$datos['email'])){
+            
+            $json = array(
+             'status'=>404,
+             'detalle'=>'Error en el campo email, coloca un email valido'
+                
+            );
+            
+            echo json_encode($json, true);   
+            
+            return;
+        } 
 
 
         $json = array(
